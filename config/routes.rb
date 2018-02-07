c6f05a22700a0e23fa33ec81ccfdd512e2945cc2
Roberto Sante <robertosante@gmail.com>
2018-02-07 13:10:22 -0700

    added form for games and redirect log and history accordingly

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :games
   devise_for :users
   root to: "home#index"
-  get '/history', to: 'home#history'
-  get '/log',     to: 'home#log'
+  get '/history', to: 'games#index'
+  get '/log',     to: 'games#new'
 end
