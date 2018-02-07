6cf57865ac77d9215a551fd5b3127c0b92a9d971
Roberto Sante <robertosante@gmail.com>
2018-02-07 13:00:58 -0700

    added validations to models

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,7 @@
 class Game < ActiveRecord::Base
+    belongs_to :user
+    belongs_to :challenger, class_name: 'User'
+
+    validates_presence_of  :user, :challenger, :user_score, :challenger_score, :round_date
+    validates_numericality_of :user_score, :challenger_score, only_integer: true, greater_than_or_equal: 0
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :trackable, :validatable
+
+  has_many :rounds
+
+  scope :challengers, ->(user) { where("id != ?", user) }
 end
